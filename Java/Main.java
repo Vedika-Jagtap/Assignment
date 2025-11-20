d5c33f01f9e820548f22a87d5307a880e5a7387d
Vedika Jagtap <jagtapvedika38@gmail.com>
2025-11-20 17:52:41 +0530

    Java practice Day 1

---
+++ Java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        /*Main m = new Main();
+
+        int code = m.hashCode();
+
+        System.out.println(code);*/    
+
+        A a = new A();
+
+        int code = a.hashCode();
+
+        System.out.println(code);
+    }
+}
